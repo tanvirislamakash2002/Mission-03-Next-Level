507950ec69107bc9a0577f8a2a6716b91d3d9956
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-19 23:31:31 +0600

    created post route using express js

--- Module 12/src/server.ts
+++ Module 12/src/server.ts
@@ -6,6 +6,13 @@ app.get('/', (req: Request, res: Response) => {
     res.send('The server is on the go!')
 })
 
+app.post('/', (req: Request, res: Response) => {
+    console.log(req);
+    res.status(201).json({
+        success: true,
+        message: 'API is working'
+    })
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
