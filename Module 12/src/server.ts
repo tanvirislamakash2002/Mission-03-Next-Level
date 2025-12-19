e15d82eed9599bc9ce6121b253dd124f372db3a2
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-20 00:34:42 +0600

    connected neon db to the server

--- Module 12/src/server.ts
+++ Module 12/src/server.ts
@@ -1,10 +1,32 @@
 import express, { Request, Response } from 'express'
+import { Pool } from "pg"
 const app = express()
 const port = 5000
 
 // parser
 app.use(express.json())
+// app.use(express.urlencoded())
 
+// DDB
+const pool = new Pool({
+    connectionString: `postgresql://neondb_owner:npg_Z6vDMwKdI1SY@ep-fancy-dawn-a4gbx7ph-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+})
+
+const initDB = async () => {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone  VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )`)
+}
+
+initDB()
 
 app.get('/', (req: Request, res: Response) => {
     res.send('The server is on the go!')
