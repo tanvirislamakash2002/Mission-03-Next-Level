e207b524d129ca8a7d09336b3322d0c775cc31bd
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-19 23:37:32 +0600

    used parser to get the body data

--- Module 12/src/server.ts
+++ Module 12/src/server.ts
@@ -2,12 +2,16 @@ import express, { Request, Response } from 'express'
 const app = express()
 const port = 5000
 
+// parser
+app.use(express.json())
+
+
 app.get('/', (req: Request, res: Response) => {
     res.send('The server is on the go!')
 })
 
 app.post('/', (req: Request, res: Response) => {
-    console.log(req);
+    console.log(req.body);
     res.status(201).json({
         success: true,
         message: 'API is working'
