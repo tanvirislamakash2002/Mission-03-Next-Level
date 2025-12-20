a00496000ac8e42a3da2d6ef4dc769c0c5725044
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-20 17:14:13 +0600

    get single data form neon db

--- Module 12/src/server.ts
+++ Module 12/src/server.ts
@@ -89,6 +89,31 @@ app.get('/users', async (req: Request, res: Response) => {
         })
     }
 })
+
+// single user
+app.get("/users/:id", async (req: Request, res: Response) => {
+    console.log(req.params.id);
+    try {
+        const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [req.params.id])
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        } else {
+            res.status(200).json({
+                success: true,
+                message: "User fetched successfully",
+                data: result.rows[0]
+            })
+        }
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+    }
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
