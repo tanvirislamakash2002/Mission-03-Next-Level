33b58157bfeaee4ff2a78925c154ecc534accce3
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-20 13:04:39 +0600

    create todo database using pg

--- Module 12/src/server.ts
+++ Module 12/src/server.ts
@@ -1,5 +1,10 @@
 import express, { Request, Response } from 'express'
 import { Pool } from "pg"
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.join(process.cwd(), '.env') })
+
 const app = express()
 const port = 5000
 
@@ -9,7 +14,7 @@ app.use(express.json())
 
 // DDB
 const pool = new Pool({
-    connectionString: `postgresql://neondb_owner:npg_Z6vDMwKdI1SY@ep-fancy-dawn-a4gbx7ph-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+    connectionString: `${process.env.CONNECTION_STR}`
 })
 
 const initDB = async () => {
@@ -24,6 +29,19 @@ const initDB = async () => {
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
         )`)
+
+    await pool.query(`
+            CREATE TABLE IF NOT EXISTS todos(
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+            title VARCHAR(200) NOT NULL,
+            description TEXT,
+            completed BOOLEAN DEFAULT false,
+            due_date DATE,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `)
 }
 
 initDB()
