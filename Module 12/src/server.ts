0027a4b29e0c5a199d985d13d721f530e8e3b8c1
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-20 20:22:00 +0600

    successfully implemented delete methhod form the neon db

--- Module 12/src/server.ts
+++ Module 12/src/server.ts
@@ -114,6 +114,31 @@ app.get("/users/:id", async (req: Request, res: Response) => {
         })
     }
 })
+
+app.put("/users/:id", async (req: Request, res: Response) => {
+    // console.log(req.params.id);
+    const { name, email } = req.body;
+    try {
+        const result = await pool.query(`UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`, [name, email, req.params.id])
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        } else {
+            res.status(200).json({
+                success: true,
+                message: "User updated successfully",
+                data: result.rows[0]
+            })
+        }
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+    }
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
