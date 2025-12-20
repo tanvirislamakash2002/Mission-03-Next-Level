52f909ef297ac27796e76696eb5c015540812afc
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-20 16:27:37 +0600

    get all user data form neon db

--- Module 12/src/server.ts
+++ Module 12/src/server.ts
@@ -50,12 +50,44 @@ app.get('/', (req: Request, res: Response) => {
     res.send('The server is on the go!')
 })
 
-app.post('/', (req: Request, res: Response) => {
-    console.log(req.body);
-    res.status(201).json({
-        success: true,
-        message: 'API is working'
-    })
+// users CRUD
+app.post('/users', async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+
+    try {
+        const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email])
+        // console.log(result.rows[0]);
+        // res.send({ message: 'data inserted' })
+        res.status(201).json({
+            success: false,
+            message: "Data Inserted Successfully",
+            data: result.rows[0]
+        })
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+
+})
+
+app.get('/users', async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`SELECT * FROM users`)
+
+        res.status(200).json({
+            success: true,
+            message: "Users retrieved successfully",
+            data: result.rows
+        })
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err
+        })
+    }
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
