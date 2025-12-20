494a47b8bef0829ab6b1a352b822c13344179a1e
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-20 22:10:30 +0600

    created functionality to get todos

--- Module 12/src/server.ts
+++ Module 12/src/server.ts
@@ -182,6 +182,25 @@ app.post('/todos', async (req: Request, res: Response) => {
         })
     }
 })
+
+app.get('/todos', async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`SELECT * FROM todos`)
+
+        res.status(200).json({
+            success: true,
+            message: "todos retrieved successfully",
+            data: result.rows
+        })
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err
+        })
+    }
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
