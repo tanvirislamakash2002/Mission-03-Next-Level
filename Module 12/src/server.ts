cc2a082047c6a26d47a8340ea5f7ff8960b9e09d
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-20 21:29:39 +0600

    added post method for todo list

--- Module 12/src/server.ts
+++ Module 12/src/server.ts
@@ -163,6 +163,25 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
         })
     }
 })
+
+// todos crud
+app.post('/todos', async (req: Request, res: Response) => {
+    const { user_id, title } = req.body;
+
+    try {
+        const result = await pool.query(`INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`, [user_id, title])
+        res.status(201).json({
+            success: true,
+            message: "Todo created",
+            data: result.rows[0]
+        })
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
