ec5701f667c8b9ffed9884128a5a81630696cb61
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-22 00:01:06 +0600

    created seperate get user controller function

--- Module 13/src/modules/user/user.controller.ts
+++ Module 13/src/modules/user/user.controller.ts
@@ -1,14 +1,12 @@
 import { Request, Response } from "express";
 import { pool } from "../../config/db";
+import { userServices } from "./user.service";
 
 const createUser = async (req: Request, res: Response) => {
     const { name, email } = req.body;
 
     try {
-        const result = await pool.query(
-            `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
-            [name, email]
-        );
+        const result = await userServices.createUser(name, email);
         // console.log(result.rows[0]);
         res.status(201).json({
             success: true,
@@ -23,6 +21,24 @@ const createUser = async (req: Request, res: Response) => {
     }
 }
 
+const getUser = async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`SELECT * FROM users`);
+
+        res.status(200).json({
+            success: true,
+            message: "Users retrieved successfully",
+            data: result.rows,
+        });
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err,
+        });
+    }
+}
 export const userControllers = {
-    createUser
+    createUser,
+    getUser
 }
--- Module 13/src/modules/user/users.routes.ts
+++ Module 13/src/modules/user/users.routes.ts
@@ -6,22 +6,6 @@ const router = express.Router()
 
 router.post('/', userControllers.createUser)
 
-router.get('/', async (req: Request, res: Response) => {
-    try {
-        const result = await pool.query(`SELECT * FROM users`);
-
-        res.status(200).json({
-            success: true,
-            message: "Users retrieved successfully",
-            data: result.rows,
-        });
-    } catch (err: any) {
-        res.status(500).json({
-            success: false,
-            message: err.message,
-            details: err,
-        });
-    }
-})
+router.get('/', userControllers.getUser)
 
 export const userRoutes = router;
