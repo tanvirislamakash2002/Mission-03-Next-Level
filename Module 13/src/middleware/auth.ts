c49e72aeeccba69c744b12c807cf34031a11b73f
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-23 11:32:40 +0600

    used jwt token to verify user

--- Module 13/src/middleware/auth.ts
+++ Module 13/src/middleware/auth.ts
@@ -1,19 +1,27 @@
 // higher order function returns a function
 
 import { NextFunction, Request, Response } from "express"
-import  jwt  from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config";
 
 const auth = () => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        const token = req.headers.authorization;
-        console.log({ authToken: token });
-        if (!token) {
-            return res.status(500).json({ message: "You are not allowed!!" })
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const token = req.headers.authorization;
+            console.log({ authToken: token });
+            if (!token) {
+                return res.status(500).json({ message: "You are not allowed!!" })
+            }
+            const decoded = jwt.verify(token, config.jwtSecret as string)
+            console.log({ decoded });
+            req.user = decoded as JwtPayload;
+            next()
+        } catch (err: any) {
+            res.status(500).json({
+                success: false,
+                message: err.message
+            })
         }
-        const decoded = jwt.verify(token, config.jwtSecret as string)
-        console.log({ decoded });
-        next()
     }
 }
 
